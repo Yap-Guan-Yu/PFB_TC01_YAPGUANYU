3420a0061bef151543a482de76a52588798f6532
Yap Guan Yu <s10258186@connect.np.edu.sg>
2024-01-29 13:18:47 +0800

    final submission

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import cash_on_hand, overheads, profits_and_loss
+import cash_on_hand, overheads, profit_loss
 
 def main():
     overheads.overhead_function()  
     cash_on_hand.coh_function()    
-    profits_and_loss.profitloss_function()        
+    profit_loss.profitloss_function()        
 
 main()
